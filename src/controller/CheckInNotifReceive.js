de4d23e9950b9588afba64c7fd7b9de6ff9db689
GISERVIN TIFIRA ZAIN <giservin.zain@sigma.co.id>
2023-11-02 09:59:37 +0700

    commit laptop kantor 2/11/23 #2

--- src/controller/CheckInNotifReceive.js
+++ src/controller/CheckInNotifReceive.js
@@ -5,39 +5,37 @@ module.exports = async (request, context) => {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const username = request.query.get("username");
-    const method = request.query.get("method");
-    const notificationRecipient = new SuperNotificationQueue(`https://rgsuperappa6e1.queue.core.windows.net/`)
-
+    // const method = request.query.get("method");
+    const notificationRecipient = new SuperNotificationQueue(`https://rgsuperappa6e1.queue.core.windows.net/`);
+    const queueName = username.replace(/[^a-zA-Z0-9]/g,'');
     try {
-        if(method == "create") {
-            await notificationRecipient.createNotificationUser(username);
+        if(await notificationRecipient.exist(queueName) == false) {
             return {
                 headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({notifCreated: true})
+                body: JSON.stringify([])
             }
         }
-        context.log(await notificationRecipient.exist(username));
-        if(await notificationRecipient.exist(username) == false) {
-            return {
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({notifExist: false})
-            }
+        const messages = await notificationRecipient.receiveNotification(queueName);
+        // context.log(messages);
+        return {
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(messages)
         }
-        if(method == "add") {
-            await notificationRecipient.addNotification(username, {meeting: "001",title: "meeting probis"});
-            return {
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({notifAdded: true})
-            }
-        }
-        if(method == "receive") {
-            const messages = await notificationRecipient.receiveNotification(username);
-            context.log(messages);
-            return {
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify(messages)
-            }
-        }
+        // if(method == "add") {
+        //     await notificationRecipient.addNotification(username, {meeting: "001",title: "meeting probis"});
+        //     return {
+        //         headers: { "Content-Type": "application/json"},
+        //         body: JSON.stringify({notifAdded: true})
+        //     }
+        // }
+        // if(method == "receive") {
+        //     const messages = await notificationRecipient.receiveNotification(username);
+        //     context.log(messages);
+        //     return {
+        //         headers: { "Content-Type": "application/json"},
+        //         body: JSON.stringify(messages)
+        //     }
+        // }
     } catch (error) {
         context.log(`error = ${error}`);
         return {
